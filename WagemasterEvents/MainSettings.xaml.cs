f2873e8abb4af4723ff6c8b11c07af4233a532c2
John Gachui <johngachui@hotmail.com>
2023-05-24 13:49:38 +0300

    Server selection

--- WagemasterEvents/MainSettings.xaml.cs
+++ WagemasterEvents/MainSettings.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using WagemasterEvents.Database;
+using System.IO;
 
 namespace WagemasterEvents
 {
@@ -20,18 +23,40 @@ namespace WagemasterEvents
         private void LoadSettings()
         {
             var settings = SettingsRepository.GetSettings();
-            ServerTextBox.Text = settings.Server;
+            ServerComboBox.ItemsSource = GetServerNamesFromDatabasePaths(ReadIniFile());
+            ServerComboBox.Text = settings.Server;
             CacheTimeTextBox.Text = settings.CacheTime.ToString();
             UsernameTextBox.Text = settings.Username;
             PasswordTextBox.Password = settings.Password;
         }
 
+        private List<string> GetServerNamesFromDatabasePaths(List<string> databasePaths)
+        {
+            List<string> serverNames = new List<string>();
+
+            foreach (var path in databasePaths)
+            {
+                if (path.StartsWith("\\\\"))
+                {
+                    var serverName = path.Split('\\')[2];
+                    if (!serverNames.Contains(serverName))
+                        serverNames.Add(serverName);
+                }
+                else
+                {
+                    if (!serverNames.Contains("localhost"))
+                        serverNames.Add("localhost");
+                }
+            }
+
+            return serverNames;
+        }
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             if (int.TryParse(CacheTimeTextBox.Text, out _))  //If the input cannot be parsed into an integer
             {
                 // Save settings to the database here
-                var server = ServerTextBox.Text;
+                var server = ServerComboBox.Text;
                 var cacheTime = int.Parse(CacheTimeTextBox.Text);
                 var username = !string.IsNullOrEmpty(UsernameTextBox.Text) ? UsernameTextBox.Text : "Username";
                 var password = !string.IsNullOrEmpty(PasswordTextBox.Password) ? PasswordTextBox.Password : "Password";
@@ -53,5 +78,35 @@ namespace WagemasterEvents
         {
             Close();
         }
+
+        private List<string> ReadIniFile()
+        {
+            // Construct the INI file path
+            string iniPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "WagemasterPayroll",
+                "WagemasterPayroll.ini"
+            );
+
+            // Read the contents of the INI file
+            string[] lines = File.ReadAllLines(iniPath, Encoding.Default);
+            List<string> databasePaths = new List<string>();
+
+            // Loop through each line and extract the database paths
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    string path = line.Substring(1, line.Length - 2);
+                    path = Path.Combine(path, "Wagemaster_data.mdb");
+
+                    databasePaths.Add(path);
+                }
+            }
+
+            return databasePaths;
+        }
+
+
     }
 }
