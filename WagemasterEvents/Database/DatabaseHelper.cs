c7add1d0ff683f1bd14a2dbc583a72b1b4d5d0d3
John Gachui <johngachui@hotmail.com>
2023-05-25 08:57:14 +0300

    trycatch in databasehelper. Content and build options for icon and Wagemaster.db

--- WagemasterEvents/Database/DatabaseHelper.cs
+++ WagemasterEvents/Database/DatabaseHelper.cs
@@ -9,7 +9,7 @@ namespace WagemasterEvents.Database
     public class DatabaseHelper
     {
         public static string ConnectionString => $"Data Source={AppDomain.CurrentDomain.BaseDirectory}WagemasterEvents.db;";
-
+        
         public static void InitializeDatabase()
         {
             //Debug.WriteLine($"Database file path: {ConnectionString}");
@@ -17,54 +17,70 @@ namespace WagemasterEvents.Database
             {
                 connection.Open();
 
-                // Check if the tables exist
-                using (var command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' AND name='EventsList'", connection))
+                try
                 {
-                    var tableExists = command.ExecuteScalar() != null;
-
-                    if (!tableExists)
+                    // Check if the tables exist
+                    using (var command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' AND name='EventsList'", connection))
                     {
-                        //Debug.WriteLine($"Table  does not exist");
-                        // Create the EventsList table
-                        var createTableSql = "CREATE TABLE EventsList (IDX INTEGER PRIMARY KEY AUTOINCREMENT,ID INTEGER,  Company TEXT, ReminderType TEXT, Reminder TEXT, Refno TEXT, Refname TEXT, DueDate TEXT, DatabasePath TEXT, NextReminderDate TEXT, Dismissed INTEGER)";
-                        try
-                        {
-                            connection.Execute(createTableSql);
-                        }
-                        catch (Exception ex)
-                        {
-                            Debug.WriteLine($"Error creating EventsList table: {ex.Message}");
-                        }
+                        var tableExists = command.ExecuteScalar() != null;
 
-                        //using (var createTableCommand = new SQLiteCommand(
-                        //    "CREATE TABLE EventsList (Company TEXT, ReminderType TEXT, Reminder TEXT, Refno TEXT, Refname TEXT, DueDate TEXT, DatabasePath TEXT, NextReminderDate TEXT, Dismissed INTEGER)", connection))
-                        //{
-                        //    createTableCommand.ExecuteNonQuery();
-                        // }
+                        if (!tableExists)
+                        {
+                            //Debug.WriteLine($"Table  does not exist");
+                            // Create the EventsList table
+                            var createTableSql = "CREATE TABLE EventsList (IDX INTEGER PRIMARY KEY AUTOINCREMENT,ID INTEGER,  Company TEXT, ReminderType TEXT, Reminder TEXT, Refno TEXT, Refname TEXT, DueDate TEXT, DatabasePath TEXT, NextReminderDate TEXT, Dismissed INTEGER)";
+                            try
+                            {
+                                connection.Execute(createTableSql);
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine($"Error creating EventsList table: {ex.Message}");
+                            }
+
+                            //using (var createTableCommand = new SQLiteCommand(
+                            //    "CREATE TABLE EventsList (Company TEXT, ReminderType TEXT, Reminder TEXT, Refno TEXT, Refname TEXT, DueDate TEXT, DatabasePath TEXT, NextReminderDate TEXT, Dismissed INTEGER)", connection))
+                            //{
+                            //    createTableCommand.ExecuteNonQuery();
+                            // }
+                        }
                     }
                 }
-
-                using (var command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' AND name='Settings'", connection))
+                catch (Exception ex)
                 {
-                    var tableExists = command.ExecuteScalar() != null;
+                    Debug.WriteLine($"Error creating EventsList table: {ex.Message}");
+                }
 
-                    if (!tableExists)
+                try
+                {
+
+                    using (var command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' AND name='Settings'", connection))
                     {
-                        // Create the Settings table
-                        using (var createTableCommand = new SQLiteCommand(
-                            "CREATE TABLE Settings (Id INTEGER PRIMARY KEY AUTOINCREMENT, Server TEXT, [Cache Time] INTEGER, Username TEXT, Password TEXT)", connection))
-                        {
-                            createTableCommand.ExecuteNonQuery();
+                        var tableExists = command.ExecuteScalar() != null;
 
-                            // Insert default values
-                            using (var insertCommand = new SQLiteCommand(
-                                "INSERT INTO Settings (Server, [Cache Time], Username, Password) VALUES ('localhost', 180,'Username','Password')", connection))
+                        if (!tableExists)
+                        {
+                            // Create the Settings table
+                            using (var createTableCommand = new SQLiteCommand(
+                                "CREATE TABLE Settings (Id INTEGER PRIMARY KEY AUTOINCREMENT, Server TEXT, [Cache Time] INTEGER, Username TEXT, Password TEXT)", connection))
                             {
-                                insertCommand.ExecuteNonQuery();
+                                createTableCommand.ExecuteNonQuery();
+
+                                // Insert default values
+                                using (var insertCommand = new SQLiteCommand(
+                                    "INSERT INTO Settings (Server, [Cache Time], Username, Password) VALUES ('localhost', 180,'Username','Password')", connection))
+                                {
+                                    insertCommand.ExecuteNonQuery();
+                                }
                             }
                         }
                     }
                 }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error creating Settings table: {ex.Message}");
+                }
+
             }
         }
 
